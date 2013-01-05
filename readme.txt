97413ce9f43e1c8e2f01233852e4dbf3f66e1415
jeffbullins <jeffbullins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-05 21:23:13 +0000

    fixed regular expression on preg_match of url
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-landing-page-and-coming-soon-page/trunk@648454 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.thinklandingpages.com
 Tags: landing page, coming soon page, maintenance page, email list, squeeze page, list building, 
 Requires at least: 3.0
 Tested up to: 3.4.1
-Stable tag: 1.1.1
+Stable tag: 1.1.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -72,4 +72,7 @@ Yes, this is a plugin and will work with your existing theme.
 
 = 1.1.1 =
 * Fixed links to plugin site
+
+= 1.1.2 =
+* Fixed regular expression for preg_match on url
 
--- ultimate-landing-page.php
+++ ultimate-landing-page.php
@@ -3,7 +3,7 @@
 Plugin Name: Ultimate Landing Page and Coming Soon Page
 Plugin URI: http://www.thinklandingpages.com
 Description: Create a landing page, coming soon page, or maintenance page and collect emails.
-Version: 1.1.1
+Version: 1.1.2
 Author: Jeff Bullins
 Author URI: http://www.thinklandingpages.com
 */
@@ -399,7 +399,7 @@ function ulp_render_form() {
 							<option disabled='disabled' value='mailchimp' <?php selected('mailchimp', $options['drp_select_box']); ?>>Mailchimp</option>
 							
 						</select>
-						<span style="color:#666666;margin-left:2px;">Aweber, MailChimp, and other email list management software are supported in Ultimate Landing Page Advanced</span></br>
+						<span style="color:#666666;margin-left:2px;">Aweber, MailChimp, Constant Contact, GetResponse, and other email list management software are supported in Ultimate Landing Page Advanced</span></br>
 						<span style="color:#666666;margin-left:2px;"><a href="http://www.thinklandingpages.com">Upgrade Now</a></span>
 					</td>
 				</tr>
@@ -469,7 +469,8 @@ $my_options = get_option('ulp_options');
 global $wp;
 //$mypage = $wp->query_vars["pagename"] == $my_options['url-slug'];
 //	if($mypage){
-	if(preg_match('/^\/\b'.$my_options['url-slug'].'\b$/', $_SERVER['REQUEST_URI'], $match)){
+//	if(preg_match('/^\/\b'.$my_options['url-slug'].'\b$/', $_SERVER['REQUEST_URI'], $match)){
+      if(preg_match('#^/'.$my_options['url-slug'].'$#', $_SERVER['REQUEST_URI'], $match)){
 		$file = plugin_dir_path(__FILE__).'template/template1.php';
 	                        //include($file);
 	                        include_wordpress_template($file);
