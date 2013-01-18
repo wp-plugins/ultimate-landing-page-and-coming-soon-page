0ac45aae433f28b616036c4d8ddd717a0b353362
jeffbullins <jeffbullins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-18 06:17:29 +0000

    Updated version number to 1.1.3 in main plugin file
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-landing-page-and-coming-soon-page/trunk@654717 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ultimate-landing-page.php
+++ ultimate-landing-page.php
@@ -3,7 +3,7 @@
 Plugin Name: Ultimate Landing Page and Coming Soon Page
 Plugin URI: http://www.thinklandingpages.com
 Description: Create a landing page, coming soon page, or maintenance page and collect emails.
-Version: 1.1.2
+Version: 1.1.3
 Author: Jeff Bullins
 Author URI: http://www.thinklandingpages.com
 */
